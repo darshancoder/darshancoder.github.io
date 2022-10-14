034ef190dec60a68e7777156032b3773386fc6f7
Darshan Kale <darshankale52@gmail.com>
2022-10-14 11:35:17 +0530

    day-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setcount] = useState(0)
   return (
     <div className="App">
-     Hello Wolrd
+     Hello Wolrd {count}
+     <button onClick={setcount(count+1)}>Count</button>
     </div>
   );
 }
